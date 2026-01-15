ea0e8d58b324896d0e114efb74f61c38cdd1ed41
Jabagh Learning Wordpress+github <jabaghsaeedjabagh@gmail.com>
2026-01-16 01:52:10 +0300

    color changed

--- build/custom-design-block/index.asset.php
+++ build/custom-design-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '28b66ea8230dee5c3927');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0b267753014cf23cc2bf');
